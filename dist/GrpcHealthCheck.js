386f2b0b23bf49fa8e4e6fe982ee12427966e3fd
Efra Peralta <efra23peralta@gmail.com>
2022-08-31 08:38:23 -0400

    chore: export files

---
+++ dist/GrpcHealthCheck.js
@@ -0,0 +1,58 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GrpcHealthCheck = void 0;
+const grpc_boom_1 = __importDefault(require("grpc-boom"));
+const HealthCheckResponse_1 = require("./proto/grpc/health/v1/HealthCheckResponse");
+class GrpcHealthCheck {
+    statusMap;
+    watchStatusMap = {};
+    watchErrorMap = {};
+    constructor(statusMap) {
+        this.statusMap = statusMap;
+    }
+    setStatus(service, status) {
+        this.statusMap[service] = status;
+    }
+    check(call, callback) {
+        const service = call.request.service;
+        const status = this.statusMap[service];
+        if (!status) {
+            callback(grpc_boom_1.default.notFound(`Unknown service: ${service}`), undefined);
+        }
+        else {
+            callback(undefined, { status });
+        }
+    }
+    watch(call) {
+        const service = call.request.service;
+        const interval = setInterval(() => {
+            let updatedStatus = HealthCheckResponse_1._grpc_health_v1_HealthCheckResponse_ServingStatus.SERVING;
+            if (!this.statusMap[service]) {
+                updatedStatus = HealthCheckResponse_1._grpc_health_v1_HealthCheckResponse_ServingStatus.SERVICE_UNKNOWN;
+                this.setStatus(service, updatedStatus);
+                call.write({ status: updatedStatus });
+            }
+            this.watchStatusMap[service] = updatedStatus;
+            if (!this.watchErrorMap[service]) {
+                const lastStatus = this.statusMap[service] || -1;
+                if (lastStatus !== updatedStatus) {
+                    this.setStatus(service, updatedStatus);
+                    call.write({ status: updatedStatus }, (error) => {
+                        if (error) {
+                            this.watchErrorMap[service] = error;
+                        }
+                    });
+                }
+            }
+            else {
+                clearInterval(interval);
+                call.end(this.watchErrorMap[service]);
+            }
+        }, 1000);
+    }
+}
+exports.GrpcHealthCheck = GrpcHealthCheck;
+//# sourceMappingURL=GrpcHealthCheck.js.map
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,14 +1,4 @@
-import type * as grpc from '@grpc/grpc-js';
-import { HealthCheckRequest__Output as HealthCheckRequest } from './proto/grpc/health/v1/HealthCheckRequest';
-import { _grpc_health_v1_HealthCheckResponse_ServingStatus as ServingStatus, HealthCheckResponse } from './proto/grpc/health/v1/HealthCheckResponse';
-export declare class GrpcHealthCheck {
-    private statusMap;
-    private watchStatusMap;
-    private watchErrorMap;
-    constructor(statusMap: {
-        [key: string]: ServingStatus;
-    });
-    private setStatus;
-    check(call: grpc.ServerUnaryCall<HealthCheckRequest, HealthCheckResponse>, callback: (error?: grpc.ServiceError, result?: HealthCheckResponse) => void): void;
-    watch(call: grpc.ServerWritableStream<HealthCheckRequest, HealthCheckResponse | Error>): void;
-}
+export { HealthCheckRequest__Output as HealthCheckRequest } from "./proto/grpc/health/v1/HealthCheckRequest";
+export { GrpcHealthCheck } from "./GrpcHealthCheck";
+export { ProtoGrpcType } from "./proto/health";
+export { HealthClient } from "./proto/grpc/health/v1/Health";
