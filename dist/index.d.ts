6f811f8a617af6f193c7e2290c4858704d5a4a03
Efra Peralta <efra23peralta@gmail.com>
2022-08-31 08:40:35 -0400

    chore: export response types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 export { HealthCheckRequest__Output as HealthCheckRequest } from "./proto/grpc/health/v1/HealthCheckRequest";
+export { HealthCheckResponse__Output as HealthCheckResponse } from "./proto/grpc/health/v1/HealthCheckResponse";
 export { GrpcHealthCheck } from "./GrpcHealthCheck";
 export { ProtoGrpcType } from "./proto/health";
 export { HealthClient } from "./proto/grpc/health/v1/Health";
