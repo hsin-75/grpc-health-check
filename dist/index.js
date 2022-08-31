aa7ece33d5ef390421e688494cf458b853366df1
Efra Peralta <efra23peralta@gmail.com>
2022-08-31 09:48:39 -0400

    chore: build sources

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.GrpcHealthCheck = void 0;
+exports.ServingStatus = exports.GrpcHealthCheck = void 0;
 var GrpcHealthCheck_1 = require("./GrpcHealthCheck");
 Object.defineProperty(exports, "GrpcHealthCheck", { enumerable: true, get: function () { return GrpcHealthCheck_1.GrpcHealthCheck; } });
+var HealthCheckResponse_1 = require("./proto/grpc/health/v1/HealthCheckResponse");
+Object.defineProperty(exports, "ServingStatus", { enumerable: true, get: function () { return HealthCheckResponse_1._grpc_health_v1_HealthCheckResponse_ServingStatus; } });
 //# sourceMappingURL=index.js.map
