5bc2043d9ef880dc28db6bc58ba97305422e2910
Efra Peralta <efra23peralta@gmail.com>
2022-08-31 09:36:29 -0400

    chore: export serving status

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { HealthCheckResponse__Output as HealthCheckResponse } from "./proto/grpc
 export { GrpcHealthCheck } from "./GrpcHealthCheck";
 export { ProtoGrpcType } from "./proto/health";
 export { HealthClient } from "./proto/grpc/health/v1/Health";
+export { _grpc_health_v1_HealthCheckResponse_ServingStatus as ServingStatus } from "./proto/grpc/health/v1/HealthCheckResponse";
